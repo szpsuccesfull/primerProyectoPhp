c058500a2f338929c11775ba1789c6ce788ea07d
501 <501@academicos.cesde.edu.co>
2020-02-14 21:02:28 -0500

    este es mi primer funcional

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+// como se difine los comentarios de linea
+// definimos una variable de tipo cadena texto, numero y letra
+
+$nombre= "samuel ";
+echo($nombre);
+$edad= 26; 
+echo( $edad);
+$estatura=1.70;
+$nombre= " angel ";
+echo($nombre);
+
+echo("hola mundo");
+print("<br>");
+echo("mi nombre es " .$nombre. " y la edad es : ".$edad);
+
+
+
+
+
+
+?>
